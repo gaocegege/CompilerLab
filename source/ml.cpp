267e1ee7ce3db487e5ce6fd228973f109659aad9
hcz <h.cz@qq.com>
2014-11-10 12:14:34 +0800

    move the parser to a submodule

--- source/ml.cpp
+++ source/ml.cpp
@@ -1,45 +1,18 @@
 #include <iostream>
 
-#include "ml_ast.h"
-#include "ml_syntax.h"
+#include "parser/myparser_rule.hpp"
 
-namespace mylang {
-
-const char name[] = "id";
-const char regex[] = "[A-Za-z][A-Za-z0-9_]*";
+using namespace myparser;
 
 template <>
-class RuleDef<ML_STR("id", 2)>: public RuleRegex<
-    ML_STR("id", 2),
-    ML_STR("[A-Za-z][A-Za-z0-9_]*", 22)
+class myparser::RuleDef<MP_STR("id", 2)>: public RuleRegex<
+    MP_STR("id", 2),
+    MP_STR("[A-Za-z][A-Za-z0-9_]*", 22)
 > {};
 
-template <char C>
-class tobj {
-public:
-    static const char c = C;
-};
-
-}
-
-using namespace mylang;
-
 void test() {
-    // StaticStr<'a', 'b', 'c'> a;
-
     std::cout << StaticStr<'a', 'b', 'c'>::getStr() << std::endl;
-    std::cout << ssSize("adsfs") << std::endl;
-
-    #define xx(str, len) x##len(str, 0)
-
-    #define x6(str, index) str[index], x5(str, index + 1)
-    #define x5(str, index) str[index], x4(str, index + 1)
-    #define x4(str, index) str[index], x3(str, index + 1)
-    // #define x3(str, index) str[index], x2(str, index + 1)
-    #define x2(str, index) str[index], x1(str, index + 1)
-    #define x1(str, index) str[index]
-    std::cout << tobj<"hello"[1]>::c << std::endl;
-    std::cout << ML_STR("hello", 5)::getStr() << std::endl;
+    std::cout << MP_STR("hello", 5)::getStr() << std::endl;
 }
 
 int main() {
