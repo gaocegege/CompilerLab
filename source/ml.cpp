37d5b1630dc1a2a0955233de6f5139d7679ce2aa
hcz <h.cz@qq.com>
2014-11-11 16:39:25 +0800

    add self build script

--- source/ml.cpp
+++ source/ml.cpp
@@ -1,3 +1,10 @@
+#if 0
+python gen_parser.py
+g++ -std=c++11 ml.cpp -o ml_gcc && ./ml_gcc
+clang++ -std=c++11 ml.cpp -o ml_clang && ./ml_clang
+exit
+#endif
+
 #include <iostream>
 
 #include "mylang_syntax.hpp"
