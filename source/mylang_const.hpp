dbca6f8957910b39618b72f4ad6efa24c1fdcf54
hcz <h.cz@qq.com>
2014-12-29 19:45:53 +0800

    apply std::string on consts

--- source/mylang_const.hpp
+++ source/mylang_const.hpp
@@ -13,56 +13,58 @@ using ml_byte = char;
 
 // operators
 
-const auto name_assign = "__assign";
-const auto name_assign_add = "__assign_add";
-const auto name_assign_sub = "__assign_sub";
-const auto name_assign_mul = "__assign_mul";
-const auto name_assign_div = "__assign_div";
+const std::string name_assign = "__assign";
+const std::string name_assign_add = "__assign_add";
+const std::string name_assign_sub = "__assign_sub";
+const std::string name_assign_mul = "__assign_mul";
+const std::string name_assign_div = "__assign_div";
 
-const auto name_equal = "__equal";
-const auto name_less_equal = "__less_equal";
-const auto name_greater_equal = "__greater_equal";
-const auto name_not_equal = "__not_equal";
-const auto name_less = "__less";
-const auto name_greater = "__greater";
-const auto name_in = "__in";
+const std::string name_equal = "__equal";
+const std::string name_less_equal = "__less_equal";
+const std::string name_greater_equal = "__greater_equal";
+const std::string name_not_equal = "__not_equal";
+const std::string name_less = "__less";
+const std::string name_greater = "__greater";
+const std::string name_in = "__in";
 
-const auto name_add = "__add";
-const auto name_sub = "__sub";
-const auto name_or = "__or";
-const auto name_xor = "__xor";
+const std::string name_add = "__add";
+const std::string name_sub = "__sub";
+const std::string name_or = "__or";
+const std::string name_xor = "__xor";
 
-const auto name_mul = "__mul";
-const auto name_div = "__div";
-const auto name_mod = "__mod";
-const auto name_and = "__and";
-const auto name_shl = "__shl";
-const auto name_shr = "__shr";
-const auto name_rol = "__rol";
-const auto name_ror = "__ror";
+const std::string name_mul = "__mul";
+const std::string name_div = "__div";
+const std::string name_mod = "__mod";
+const std::string name_and = "__and";
+const std::string name_shl = "__shl";
+const std::string name_shr = "__shr";
+const std::string name_rol = "__rol";
+const std::string name_ror = "__ror";
 
-const auto name_pos = "__pos";
-const auto name_neg = "__neg";
-const auto name_not = "__not";
+const std::string name_pos = "__pos";
+const std::string name_neg = "__neg";
+const std::string name_not = "__not";
 
-const auto name_prev = "__prev"; // TODO
-const auto name_next = "__next"; // TODO
+const std::string name_prev = "__prev"; // TODO
+const std::string name_next = "__next"; // TODO
 
 // builtin interfaces
 
-const auto name_array = "__array"; // arg: (...)
+const std::string name_array = "__array"; // arg: (...)
 // jump in the same block
-const auto name_goto = "__goto"; // arg: (tag)
-const auto name_branch = "__branch"; // arg: (tag, cond) // if !cond then goto(tag)
+const std::string name_goto = "__goto"; // arg: (tag)
+const std::string name_branch = "__branch"; // arg: (tag, cond) // if !cond then goto(tag)
 // jump with block switch
-const auto name_call = "__call"; // arg: (obj)
+const std::string name_call = "__call"; // arg: (obj)
 
-// values
+// builtin values
 
-const auto name_self = "__self"; // TODO
-const auto name_env = "__env"; // TODO
-const auto name_input = "__input"; // receive
-const auto name_result = "__result"; // return
+const std::string name_self = "__self"; // TODO: var list
+const std::string name_code = "__code"; // TODO: code (as constexpr)
+const std::string name_base = "__base"; // extends
+const std::string name_env = "__env"; // uses
+const std::string name_input = "__input"; // receive
+const std::string name_result = "__result"; // return
 
 }
 
